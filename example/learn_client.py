9a032e6cfd5b19c3e70bc4d2e535bf2ab84c659e
abhishek_padalkar <abhishek.padalkar@smail.inf.h-brs.de>
2019-03-14 01:03:14 +0100

    modifying example trajectory in learn client to square wave

--- example/learn_client.py
+++ example/learn_client.py
@@ -12,7 +12,9 @@ if __name__ == "__main__":
     # Generating a hypothetical trajectory
     x = np.linspace(0, 5)
     y = np.linspace(0, 5)
-    z = np.linspace(0, 5)
+    z = np.zeros(10)
+    z = np.hstack((z, np.ones(30)))
+    z = np.hstack((z, np.ones(10)*0.5))
     o_x = np.linspace(0, 1)
     o_y = np.linspace(0, 1)
     o_z = np.linspace(0, 1)
@@ -20,8 +22,9 @@ if __name__ == "__main__":
 
     req.header.frame_id = 'base_link'
     req.output_weight_file_name.data = 'example.yaml'
+    req.dmp_name.data = 'square_wave'
     req.header.stamp = rospy.Time.now()
-    req.n_bfs = 50
+    req.n_bfs = 500
     req.n_dmps = 6
     for i in range(x.shape[0]):
         pose = Pose()
