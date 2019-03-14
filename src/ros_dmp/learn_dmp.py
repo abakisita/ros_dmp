e7b1aa76236882b1892cc0c397bf89ede3ce4134
abhishek_padalkar <abhishek.padalkar@smail.inf.h-brs.de>
2019-03-14 01:04:22 +0100

    adding info msgs and handling results

--- src/ros_dmp/learn_dmp.py
+++ src/ros_dmp/learn_dmp.py
@@ -18,7 +18,7 @@ class LearnDmp:
         
         rospy.init_node("learn_dynamic_motion_primitive_service")
         service_ = rospy.Service('learn_dynamic_motion_primitive_service', LearnDMP, self.learn_dmp_handler)
-
+    
         # Publishers
         self.imitated_path_pub = rospy.Publisher("~imitated_path", Path, queue_size=1)
         self.demonstrated_path_pub = rospy.Publisher("~demonstrated_path", Path, queue_size=1)
@@ -27,23 +27,25 @@ class LearnDmp:
         self.weights_file_path = rospy.get_param('~weights_file_path', '../../data/weights/')
         loop_rate = rospy.get_param('~loop_rate')
 
+        self.result = ""
         r = rospy.Rate(loop_rate)
         rospy.spin()
 
     def learn_dmp_handler(self, req):
 
+        rospy.loginfo("Recieved request to learn a motion primitive")
         trajectory = np.zeros((6, len(req.poses)))
-        
+        rospy.loginfo("Learning motion primitive " + req.dmp_name.data)
         for i in range(len(req.poses)):
             rpy = tf.transformations.euler_from_quaternion([req.poses[i].orientation.x, req.poses[i].orientation.y,
                                                                     req.poses[i].orientation.z, req.poses[i].orientation.w])
             trajectory[:,i] = [req.poses[i].position.x, req.poses[i].position.y, req.poses[i].position.z,
                             rpy[0], rpy[1], rpy[2]]
         self.learn_dmp(trajectory, req.output_weight_file_name.data, req.n_dmps, req.n_bfs)
-        
+        rospy.loginfo("Successfully learned the motion primitive")
         # Return response 
         response = LearnDMPResponse()
-        response.result.data = "success"
+        response.result.data = self.result
         return response
 
     def learn_dmp(self, trajectory, file_name, n_dmps=6, n_bfs=50):
@@ -64,8 +66,6 @@ class LearnDmp:
         # Learn weights
         weights = self.dmp.imitate_path(y_des=trajectory)
 
-
-
         #save weights to desired file
         data = {'x': np.asarray(weights[0, :]).tolist(), 'y': np.asarray(weights[1, :]).tolist(),
                 'z': np.asarray(weights[2, :]).tolist(), 'roll': np.asarray(weights[3, :]).tolist(),
@@ -75,8 +75,10 @@ class LearnDmp:
         try:
             with open(file, "a+") as f:
                 yaml.dump(data, f)
+            self.result = "success"
         except:
             rospy.logerr("Cannot save weight file. Check if the directory of the weight file exists. Related parameter can be found in launch file.") 
+            self.result = "failed"
 
         # Imitate the same path as demonstrated        
         pos, vel, acc = self.dmp.rollout(goal=demonstrated_goal_pose, y0=demonstrated_initial_pose)
@@ -95,11 +97,11 @@ class LearnDmp:
         # Publish Demonstrated Path
         demonstrated_path = Path()
         demonstrated_path.header.frame_id = "/base_link"
-        for itr in range(demonstrated_trajectory.shape[0]):
+        for itr in range(demonstrated_trajectory.shape[1]):
             pose_stamped = PoseStamped()
-            pose_stamped.pose.position.x = demonstrated_trajectory[itr, 0]
-            pose_stamped.pose.position.y = demonstrated_trajectory[itr, 1]
-            pose_stamped.pose.position.z = demonstrated_trajectory[itr, 2]
+            pose_stamped.pose.position.x = demonstrated_trajectory[0, itr]
+            pose_stamped.pose.position.y = demonstrated_trajectory[1, itr]
+            pose_stamped.pose.position.z = demonstrated_trajectory[2, itr]
             demonstrated_path.poses.append(pose_stamped)
         self.demonstrated_path_pub.publish(demonstrated_path)
 
